fa0ab3816468faca486ab4a580a8abed3b1e7943
José Aram Méndez Gómez <104022682+Aram32mm@users.noreply.github.com>
2025-11-19 18:09:29 -0600

    our code to protect (code bank)

---
+++ code_bank/python/two_sum.py
@@ -0,0 +1,9 @@
+def two_sum(nums, target):
+    """Hash map solution"""
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []
